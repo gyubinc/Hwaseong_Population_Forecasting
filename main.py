463fd36c83d750abfc9e9e46411b0dacdd73b6ed
gyubinc <yeppi315@gmail.com>
2023-08-09 15:30:41 +0900

    feat) main file

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from code.utils import seed_everything
+from code.train import LSTM_train
+from code.arguments import get_args
 
 if __name__ == "__main__":
+    # seed 고정
     seed_everything(42)
+    args = get_args()
+    
+    LSTM_train(args)
+    print('finished')
+    
     
