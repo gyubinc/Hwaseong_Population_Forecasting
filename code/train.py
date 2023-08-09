fc9e3a091a230b595dd163839e82974c3fc15baf
gyubinc <yeppi315@gmail.com>
2023-08-09 15:30:29 +0900

    feat) train file

---
+++ code/train.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+from .model import LSTM
+from .dataset import create_data_loader
+from .preprocessing import preprocessing
+import pandas as pd
+
+class RMSELoss(nn.Module):
+    def forward(self, y_pred, y_true):
+        mse_loss = nn.functional.mse_loss(y_pred, y_true)
+        rmse_loss = torch.sqrt(mse_loss)
+        return rmse_loss
+
+
+def LSTM_train(args):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"current device: {device}")
+
+    # 데이터 준비
+    df = pd.read_excel(args.train_path)
+    df = preprocessing(df)
+    train_loader = create_data_loader(df, args.window_size, args.batch_size)
+    
+    # 모델 준비
+    model = LSTM(args.input_size, args.hidden_size, args.num_layers, args.output_size).to(device)
+    model = model.float()
+    
+    # metric, optimizer 준비
+    #criterion = nn.MSELoss()
+    criterion = RMSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    
+    
+    
+    for epoch in range(args.num_epochs):
+        for i, (inputs, labels) in enumerate(train_loader):
+            inputs = inputs.to(device)
+            labels = labels.unsqueeze(1).to(device)
+
+            # Forward
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()            
+        if (epoch+1) % 50 == 0:
+            print ('Epoch [{}/{}], Loss: {:.4f}' 
+                .format(epoch+1, args.num_epochs, loss.item()))
