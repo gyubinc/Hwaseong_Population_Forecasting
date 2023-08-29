a9c2cda09f2e1224b85e0ba683b8647ded1d9385
Mok Jinhwi <jhmok4@gmail.com>
2023-08-29 15:22:16 +0900

    AR 추가

--- EDA/Timseries Test.py
+++ EDA/Timseries Test.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 
 # plot 한글 안깨지게
+import matplotlib.pyplot as plt
 from matplotlib import font_manager, rc
 font_path = "C:/Users/jhmok/AppData/Local/Microsoft/Windows/Fonts/NanumGothic.ttf"
 font = font_manager.FontProperties(fname=font_path).get_name()
@@ -52,10 +53,14 @@ second_diff = pd.DataFrame({"second_diff" : second_diff}, index = df.index)
 df = pd.concat([df, second_diff], axis = 1)
 
 
+
 total_pop = df["총인구"]
 first_diff_pop = df["first_diff"].dropna()
 first_log_diff_pop = df["first_log_diff"].dropna()
 second_diff_pop = df["second_diff"].dropna()
+
+total_pop_df = df.loc[:,["월별", "총인구"]]
+
 
 ###########################
 
@@ -163,6 +168,66 @@ kpss_test(second_diff_pop)
 
 
 
+###########################
+
+## AR 돌려보기
+import statsmodels.api as sm
+from statsmodels.tsa.arima.model import ARIMA
+
+model = ARIMA(total_pop_df["총인구"], order = (4,1,0))
+fit_model = model.fit()
+print(fit_model.summary())
+
+
+forecast_steps = 32
+forecast = fit_model.forecast(steps=forecast_steps)
+
+print("Forecasted values:", forecast)
+
+fig, ax = plt.subplots(figsize =(6, 4))
+plt.plot(total_pop_df.index, total_pop_df["총인구"], color = "b")
+plt.plot(forecast.index, forecast.values, color = "r")
+plt.title("총인구")
+plt.show()
+
+
+##
+model = ARIMA(total_pop_df["총인구"], order = (4,2,0))
+fit_model = model.fit()
+print(fit_model.summary())
+
+
+forecast_steps = 32
+forecast = fit_model.forecast(steps=forecast_steps)
+
+print("Forecasted values:", forecast)
+
+
+fig, ax = plt.subplots(figsize =(6, 4))
+plt.plot(total_pop_df.index, total_pop_df["총인구"], color = "b")
+plt.plot(forecast.index, forecast.values, color = "r")
+plt.title("총인구(2차 차분)")
+plt.show()
+
+
+
+##
+model = ARIMA(np.log(total_pop_df["총인구"]), order = (4,1,0))
+fit_model = model.fit()
+print(fit_model.summary())
+
+
+forecast_steps = 32
+forecast = fit_model.forecast(steps=forecast_steps)
+
+print("Forecasted values:", forecast)
+
+
+fig, ax = plt.subplots(figsize =(6, 4))
+plt.plot(total_pop_df.index, total_pop_df["총인구"], color = "b")
+plt.plot(forecast.index, np.exp(forecast.values), color = "r")
+plt.title("총인구 (1차 로그 차분)")
+plt.show()
 
 
 
