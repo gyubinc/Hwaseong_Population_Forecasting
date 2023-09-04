368c268640c19f24929012e0158853e5b19d5808
gyubinc <yeppi315@gmail.com>
2023-09-04 17:41:09 +0900

    feat) 단변량 transformer dataset

--- code/dataset.py
+++ code/dataset.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader 
 import numpy as np
+from torch.utils.data import DataLoader, Dataset
+
 '''
 종속변수는 항상 마지막 열에 위치시키고 시작
 uni-variate 데이터는 무조건 one-step 기준
@@ -128,3 +130,38 @@ def Transformer_create_data_loader(df, window_size, batch_size, option, step):
     dataset = Transformer_TimeSeriesDataset(df, window_size, option, step)
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
     return data_loader        
+
+
+'''
+단변량 transformer 구현
+'''
+class windowDataset(Dataset):
+    def __init__(self, y, input_window=80, output_window=20, stride=5):
+        #총 데이터의 개수
+        L = y.shape[0]
+        #stride씩 움직일 때 생기는 총 sample의 개수
+        num_samples = (L - input_window - output_window) // stride + 1
+
+        #input과 output
+        X = np.zeros([input_window, num_samples])
+        Y = np.zeros([output_window, num_samples])
+
+        for i in np.arange(num_samples):
+            start_x = stride*i
+            end_x = start_x + input_window
+            X[:,i] = y[start_x:end_x]
+
+            start_y = stride*i + input_window
+            end_y = start_y + output_window
+            Y[:,i] = y[start_y:end_y]
+
+        X = X.reshape(X.shape[0], X.shape[1], 1).transpose((1,0,2))
+        Y = Y.reshape(Y.shape[0], Y.shape[1], 1).transpose((1,0,2))
+        self.x = X
+        self.y = Y
+        
+        self.len = len(X)
+    def __getitem__(self, i):
+        return self.x[i], self.y[i]
+    def __len__(self):
+        return self.len
