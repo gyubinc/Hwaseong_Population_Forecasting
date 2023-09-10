afa79c6218bf52b98ed8081a13b20eda19a877b6
gyubinc <yeppi315@gmail.com>
2023-09-11 06:31:31 +0900

    feat) inference preprocessing

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -15,7 +15,7 @@ def preprocessing(df, window_size, step):
     valid_df = df.iloc[-window_size-step:, :]
     return train_df, valid_df
 
-def inference_preprocessing(df, window_size, step):
+def inference_preprocessing(df, window_size):
     # 날짜를 정수로 변경
     #df['월별'] = pd.to_datetime(df['월별'])
     #df['월별'] = df['월별'].values.astype(float)
