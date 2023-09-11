8c2eaaa14d74fff16cc670e0dbd7a89e325975fa
gyubinc <yeppi315@gmail.com>
2023-09-11 21:38:54 +0900

    feat) arguments 파일

--- code/arguments.py
+++ code/arguments.py
@@ -1,34 +1,29 @@
 import argparse
 
 def get_args():
-    # 기본 설정값만 남기고, hyperparameter는 wandb에게 맡김.
+    # parser 정의
     parser = argparse.ArgumentParser()
+    
+    # 모델 정의
     parser.add_argument('--model_name', default='LSTM', type=str)
-    parser.add_argument('--learning_rate', default=1e-2, type=float)
-    parser.add_argument('--weight_decay', default=0, type=float)
-    parser.add_argument('--loss', default='L1', help="['L1','MSE','Huber','plcc'] 중 택1", type=str)
-
-    parser.add_argument('--batch_size', default=4, type=int)
-    parser.add_argument('--num_epochs', default=2000, type=int)
-    
-    parser.add_argument('--shuffle', default=True)
-    parser.add_argument('--train_path', default='./data/찐 최종 데이터.xlsx')
-    
-    parser.add_argument('--dev_path', default='./data/semi_last.xlsx')
-    parser.add_argument('--test_path', default='./data/semi_last.xlsx')
-    parser.add_argument('--predict_path', default='./data/semi_last.xlsx')
-
     parser.add_argument('--input_size', default = 50, type = int)
     parser.add_argument('--hidden_size', default = 64, type = int)
     parser.add_argument('--num_layers', default = 4, type = int)
     parser.add_argument('--output_size', default = 12, type = int)
     parser.add_argument('--window_size', default = 24, type = int)
     
+    # 모델 하이퍼 파라미터 정의
+    parser.add_argument('--learning_rate', default=1e-2, type=float)
+    parser.add_argument('--weight_decay', default=0, type=float)
+    parser.add_argument('--loss', default='L1', help="['L1','MSE','Huber','plcc'] 중 택1", type=str)
+    parser.add_argument('--batch_size', default=4, type=int)
+    parser.add_argument('--num_epochs', default=2000, type=int)
+    parser.add_argument('--shuffle', default=True)
+    
+    # 학습 옵션 정의
+    parser.add_argument('--train_path', default='./data/찐 최종 데이터.xlsx')
     parser.add_argument('--option', default = 2, type = int)
     parser.add_argument('--step', default = 12, type = int)
-    
     parser.add_argument('--inference', default = 0, type = int)
     
     return parser.parse_args()
-    # args = parser.parse_args(args=[])
-    # return args
