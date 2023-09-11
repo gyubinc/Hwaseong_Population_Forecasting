595060c1f2d80b64faecf784ed4a629b158f5c39
Mok Jinhwi <jhmok4@gmail.com>
2023-09-11 16:37:22 +0900

    Add DM test & MCS test

--- EDA/Modified DIebold Marino test.py
+++ EDA/Modified DIebold Marino test.py
@@ -1,62 +1,31 @@
 import pandas as pd
-from scipy.stats import t
+import numpy as np
+from scipy.stats import norm, t
 from statsmodels.tsa.stattools import adfuller
-import matplotlib.pyplot as plt 
 
-df1 = pd.DataFrame([-1033.137219,880.1831193,-760.8555032,-209.5581889,-378.7676186,-2495.966267,165.9144285,327.3453827,-405.4451365,-585.9153313,947.7145591,274.2871115
-])
-df2 = pd.DataFrame([1867.994141,-401.6391602,-1227.361084,462.5367126,-2267.583252,-2373.152954,338.1312866,63.38195801,194.2364502,-680.7015686,2787.735229,38.76818848
-])
-
-
-def gamma_k(df1, df2, k) :
-    n = len(df1)
-    d_bar = (df1**2 - df2**2).mean()[0]
-    d = []
-    for i in range(0, n) :
-        diff = df1[0][i]**2 - df2[0][i]**2
-        d.append(diff)
+def check_assumption(f1, f2, y, loss_type = "SE") :
+    # Calculate errors
+    e1 = f1 - y
+    e2 = f2 - y
     
-    gamma = 0
-    for t in range(k, n) :
-        gamma += ((d[t] - d_bar)*(d[t-k]- d_bar)) * 1/n
-    return gamma
-
-
-
-def var_d(df1, df2) :
-    n = len(df1)
-    h = int(n**(1/3) + 1)
-    gamma_0 = gamma_k(df1, df2, 0)
-    gamma_sum = 0  # Initialize the sum
-    for k in range(1, h):
-        gamma_sum += gamma_k(df1, df2, k)
-    result = 1/n * (gamma_0 + 2*gamma_sum)
-    return result
-
-
-
-def test_statistic(df1, df2) :
-    n = len(df1)
-    h = int(n**(1/3) + 1)
-    d_bar = (df1**2 - df2**2).mean()[0]
-    cons = ((n + 1 - 2*h + 1/n*h*(h-1))/n)**(1/2)
-    s1 = var_d(df1, df2)**(-1/2) * (df1**2 - df2**2).mean()[0]
-    s1_star = cons*s1
-    return s1_star
-
-
-def MDM_test(df1, df2) :
-    n = len(df1)
-    p_value = t.cdf(test_statistic(df1, df2), n-1)
-    if p_value < 0.05 :
-        print(f"방법1의 예측 정확도가 방법2의 예측정확도보다 통계적으로 유의하게 높습니다.")
-    else :
-        print(f"방법1의 예측 정확도와 방법2의 예측정확도는 차이가 없습니다.")
+    # Calculate loss based on specified loss type
+    if loss_type == "SE":
+        g1 = e1 ** 2
+        g2 = e2 ** 2
+    elif loss_type == "AE":
+        g1 = np.abs(e1)
+        g2 = np.abs(e2)
+    elif loss_type == "SPE":
+        g1 = ((y - f1) / f1) ** 2
+        g2 = ((y - f2) / f2) ** 2
+    elif loss_type == "ASE":
+        g1 = np.abs(e1[1:]) / np.mean(np.abs(np.diff(y)))
+        g2 = np.abs(e2[1:]) / np.mean(np.abs(np.diff(y)))
+    elif isinstance(loss_type, (int, float)):
+        g1 = np.exp(loss_type * e1) - 1 - loss_type * e1
+        g2 = np.exp(loss_type * e2) - 1 - loss_type * e2
         
-
-def check_assumption(df1, df2) :
-    data = df1**2 - df2**2 
+    data = g1 - g2
     result = adfuller(data)
     adf_statistic, p_value = result[0], result[1]
     if p_value <= 0.05:
@@ -64,12 +33,102 @@ def check_assumption(df1, df2) :
     else:
         print(f"기본 가정을 만족하지 않는 적절하지 않은 검정입니다.")
 
-
-check_assumption(df1, df2)
-MDM_test(df1, df2)
-
-
-
-
+def DM_test(f1, f2, y, loss_type="SE", h=1, c=False, H1="same"):
+    # Calculate errors
+    e1 = f1 - y
+    e2 = f2 - y
     
+    # Calculate loss based on specified loss type
+    if loss_type == "SE":
+        g1 = e1 ** 2
+        g2 = e2 ** 2
+    elif loss_type == "AE":
+        g1 = np.abs(e1)
+        g2 = np.abs(e2)
+    elif loss_type == "SPE":
+        g1 = ((y - f1) / f1) ** 2
+        g2 = ((y - f2) / f2) ** 2
+    elif loss_type == "ASE":
+        g1 = np.abs(e1[1:]) / np.mean(np.abs(np.diff(y)))
+        g2 = np.abs(e2[1:]) / np.mean(np.abs(np.diff(y)))
+    elif isinstance(loss_type, (int, float)):
+        g1 = np.exp(loss_type * e1) - 1 - loss_type * e1
+        g2 = np.exp(loss_type * e2) - 1 - loss_type * e2
+    
+    # Calculate d and other statistics
+    d = g1 - g2
+    T = len(d)
+    dbar = np.mean(d)
+    
+    def gammahat(k):
+        temp1 = d - dbar
+        temp2 = np.concatenate([np.full(abs(k), np.nan), temp1])[:T]
+        temp2 -= dbar
+        temp = temp1 * temp2
+        temp = temp[abs(k):]
+        return np.sum(temp) / T
+    
+    # Calculate gdk
+    if h > 1:
+        gdk = np.array([gammahat(k) for k in range(1, h)])
+    else:
+        gdk = 0
+    
+    gdk = gammahat(0) + 2 * np.sum(gdk)
+    
+    # Calculate DM statistic
+    DM = dbar / np.sqrt(gdk / T)
+    
+    # Calculate p-value based on alternative hypothesis
+    if H1 == "same":
+        pval = 2 * min(norm.sf(DM), norm.cdf(DM))
+    elif H1 == "less":
+        pval = norm.sf(DM)
+    elif H1 == "more":
+        pval = norm.cdf(DM)
+        
+    # Apply Harvey-Leybourne-Newbold correction for small samples if c=True
+    if c:
+        DM *= np.sqrt((T + 1 - 2 * h + h * (h - 1)) / T)
+        if H1 == "same":
+            pval = 2 * min(1 - t.cdf(DM, df=T - 1), t.cdf(DM, df=T - 1))
+        elif H1 == "less":
+            pval = 1 - t.cdf(DM, df=T - 1)
+        elif H1 == "more":
+            pval = t.cdf(DM, df=T - 1)
+    
+    # Formulate alternative hypothesis statement
+    if H1 == "same":
+        alt = "Forecast f1 and f2 have different accuracy."
+    elif H1 == "less":
+        alt = "Forecast f1 is less accurate than f2."
+    elif H1 == "more":
+        alt = "Forecast f1 is more accurate than f2."
+    
+    
+    result = {
+        "statistic": DM,
+        "parameter": h,
+        "alternative": alt,
+        "p_value": pval,
+        "method": "Diebold-Mariano test",
+        "data_name": f"f1 and f2 and y"
+    }
+    
+    return result
 
+
+
+
+df = pd.read_excel("./data/최종 예측값 테이블.xlsx")
+df_sort = df.sort_values(by = "RMSE")
+
+df0 = np.array(df_sort.iloc[-1, 1:13])
+df1 = np.array(df_sort.iloc[0, 1:13])
+df2 = np.array(df_sort.iloc[1, 1:13])
+
+
+
+result = DM_test(df1, df2, df0, loss_type = "SE", h=1, c=True, H1="more")
+check_assumption(df1, df2, df0)
+print("p-value", result["p_value"])
